4eaec59ee7e4bb92da3c9676891e3d6077fed726
Najla <najlaalthobaiti8@gmail.com>
2024-07-01 17:38:27 +0300

    button grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,26 @@
   </title>
 </svelte:head>
    <div class=" bg-slate-200 h-[30rem] rounded-2xl
-   w-[20rem] flex justify-center items-center">
-   <div class="bg-red-300 min h-20 w-20 rounded-full  ">
-
+   w-[20rem] grid grid-cols-4 gap-1">
+    <button> % </button>
+    <button> √ </button>
+    <button> CE </button>
+    <button> C </button>
+    <button> 7 </button>
+    <button> 8 </button>
+    <button> 9 </button>
+    <button> - </button>
+    <button> 4 </button>
+    <button> 5 </button>
+    <button> 6 </button>
+    <button> ÷ </button>
+    <button> 1 </button>
+    <button> 2 </button>
+    <button> 3 </button>
+    <button> x </button>
+    <button> . </button>
+    <button> 0 </button>
+    <button> = </button>
+    <button> + </button>
    </div>
-  </div>
+  
